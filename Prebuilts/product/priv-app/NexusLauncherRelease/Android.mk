b92f3a74466d6051d2c81098f06825c68120401b
GhostMaster69-dev <rathore6375@gmail.com>
2022-04-17 22:50:10 +0530

    NexusLauncherRelease: override Lawnchair Launcher as well
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- Prebuilts/product/priv-app/NexusLauncherRelease/Android.mk
+++ Prebuilts/product/priv-app/NexusLauncherRelease/Android.mk
@@ -7,6 +7,6 @@ LOCAL_SRC_FILES := NexusLauncherRelease.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/priv-app/NexusLauncherRelease
-LOCAL_OVERRIDES_PACKAGES := PixelLauncher TrebuchetQuickStep Home Launcher2 Launcher3 Launcher3QuickStep NexusLauncherPrebuilt NexusLauncherRelease ParanoidQuickStep Launcher3Go Launcher3QuickStepGo
+LOCAL_OVERRIDES_PACKAGES := PixelLauncher TrebuchetQuickStep Home Launcher2 Launcher3 Launcher3QuickStep NexusLauncherPrebuilt NexusLauncherRelease ParanoidQuickStep Launcher3Go Launcher3QuickStepGo Lawnchair
 
 include $(BUILD_PREBUILT)

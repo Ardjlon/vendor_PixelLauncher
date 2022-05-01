7ad406a2072047a6061fefb435f849974487a947
Ardjlon <carlosardany@gmail.com>
2022-05-01 06:19:40 +0000

    NexusLauncherRelase: Improve override packages durign build

--- Prebuilts/product/priv-app/NexusLauncherRelease/Android.mk
+++ Prebuilts/product/priv-app/NexusLauncherRelease/Android.mk
@@ -1,12 +1,36 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := Remover
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+        Home \
+        Launcher2 \
+        Launcher3 \
+        Launcher3Go \
+        Launcher3QuickStep \
+        Launcher3QuickStepGo \
+        Lawnchair \
+        NexusLauncherPrebuilt \
+        NexusLauncherRelease \
+        ParanoidQuickStep \
+        PixelLauncher \
+        TrebuchetQuickStep
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := /dev/null
+
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+
 LOCAL_MODULE := NexusLauncherReleaseMod
 LOCAL_MODULE_STEM := NexusLauncherRelease.apk
 LOCAL_SRC_FILES := NexusLauncherRelease.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/priv-app/NexusLauncherRelease
-LOCAL_OVERRIDES_PACKAGES := PixelLauncher TrebuchetQuickStep Home Launcher2 Launcher3 Launcher3QuickStep NexusLauncherPrebuilt NexusLauncherRelease ParanoidQuickStep Launcher3Go Launcher3QuickStepGo Lawnchair
 
 include $(BUILD_PREBUILT)

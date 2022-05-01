b5f5e43cddc40102dd518c5d8a09b800a16b41e0
Ardjlon <carlosardany@gmail.com>
2022-05-01 06:16:33 +0000

    Update modules name * This may fix conflicts with source roms during build

--- Prebuilts/product/overlay/Android.mk
+++ Prebuilts/product/overlay/Android.mk
@@ -3,7 +3,7 @@ include $(call first-makefiles-under,$(LOCAL_PATH))
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := ThemedIconsOverlay.apk
+LOCAL_MODULE := ThemedIconsOverlayMod
 LOCAL_MODULE_STEM := ThemedIconsOverlay.apk
 LOCAL_SRC_FILES := ThemedIconsOverlay.apk
 LOCAL_MODULE_TAGS := optional
--- Prebuilts/product/overlay/PixelRecentsProvider/Android.mk
+++ Prebuilts/product/overlay/PixelRecentsProvider/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := PixelRecentsProvider.apk
+LOCAL_MODULE := PixelRecentsProviderMod
 LOCAL_MODULE_STEM := PixelRecentsProvider.apk
 LOCAL_SRC_FILES := PixelRecentsProvider.apk
 LOCAL_MODULE_TAGS := optional

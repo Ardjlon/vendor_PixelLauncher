f35b6c825df5689b898c48d8bf04723c56f7ce55
Ardjlon <carlosardany@gmail.com>
2022-06-16 08:39:50 -0600

    ThemedIconsOverlay: Add remover for some overlays * Also fix a derp with path

--- Prebuilts/product/overlay/Android.mk
+++ Prebuilts/product/overlay/Android.mk
@@ -1,6 +1,19 @@
 include $(call first-makefiles-under,$(LOCAL_PATH))
-
 LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := IconsOverlayRemover
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+        CustomPixelLauncherOverlay
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := /dev/null
+
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := ThemedIconsOverlayMod
@@ -8,6 +21,6 @@ LOCAL_MODULE_STEM := ThemedIconsOverlay.apk
 LOCAL_SRC_FILES := ThemedIconsOverlay.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/overlay/ThemedIconsOverlay.apk
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/overlay/
 
 include $(BUILD_PREBUILT)
